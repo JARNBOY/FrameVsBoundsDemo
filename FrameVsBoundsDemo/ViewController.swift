b1a5db8bb8f638b55c4760991c921505ec9e436f
Papon <papon.s@lawyerplusgroup.com>
2023-05-13 15:06:32 +0700

    [Demo][Bounds vs Frame] create sample code for display rotate rectangle for look difference frame and bounds

--- FrameVsBoundsDemo/ViewController.swift
+++ FrameVsBoundsDemo/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var orangeView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        self.orangeView.transform = CGAffineTransform(rotationAngle: 5)
+        
+        let testView = UIView(frame: CGRect(x: self.orangeView.frame.origin.x,
+                                            y: self.orangeView.frame.origin.y,
+                                            width: self.orangeView.frame.size.width,
+                                            height: self.orangeView.frame.size.height))
+        testView.layer.borderColor = UIColor.red.cgColor
+        testView.layer.borderWidth = 2
+        self.view.addSubview(testView)
+
+        self.printAll()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    private func printAll() {
+        print("=========================")
+        print("X : ", self.orangeView.frame.origin.x)
+        print("Y : ", self.orangeView.frame.origin.y)
+        print("width : ", self.orangeView.frame.size.width)
+        print("height : ", self.orangeView.frame.size.height)
+        print("=========================")
+        print("X : ", self.orangeView.bounds.origin.x)
+        print("Y : ", self.orangeView.bounds.origin.y)
+        print("width : ", self.orangeView.bounds.size.width)
+        print("height : ", self.orangeView.bounds.size.height)
+        print("=========================")
+    }
+    
+    @IBAction func btnRotate_Click() {
+        self.orangeView.transform = CGAffineTransform(rotationAngle: 5)
+        
+        let testView = UIView(frame: CGRect(x: self.orangeView.frame.origin.x,
+                                            y: self.orangeView.frame.origin.y,
+                                            width: self.orangeView.frame.size.width,
+                                            height: self.orangeView.frame.size.height))
+        testView.layer.borderColor = UIColor.green.cgColor
+        testView.layer.borderWidth = 2
+        self.view.addSubview(testView)
+        
+        self.printAll()
+    }
 }
+
+
 
